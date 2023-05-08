ac948de21929ba022c8472244d50a1b19aa62d59
ahad3333 <akhadillokhabibullaev@gmail.com>
2023-05-08 11:40:12 +0800

    excel added

---
+++ excelbynum.go
@@ -0,0 +1,10 @@
+package excel
+
+func titleToNumber(columnTitle string) int {
+    result := 0
+    for i := 0; i < len(columnTitle); i++ {
+        value := int(columnTitle[i] - 'A' + 1)
+        result = result*26 + value
+    }
+    return result
+}
